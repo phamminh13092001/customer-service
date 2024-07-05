97318e94b48b94797c9b413918ea85cf05c2f104
phamminh13092001 <119114251+phamminh13092001@users.noreply.github.com>
2024-07-05 11:46:01 +0700

    Delete usertitle directory

--- usertitle/service/UserTitleService.java
+++
@@ -1,7 +0,0 @@
-package vn.fis.spro.customer.domains.usertitle.service;
-
-public interface UserTitleService {
-    void getTitleByUsername(String username);
-
-    void getUserTitle(String username);
-}
--- usertitle/service/impl/UserTitleServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package vn.fis.spro.customer.domains.usertitle.service.impl;
-
-import org.springframework.stereotype.Service;
-import vn.fis.spro.customer.domains.usertitle.service.UserTitleService;
-
-@Service
-public class UserTitleServiceImpl implements UserTitleService {
-}
